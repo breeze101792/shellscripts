d301e79f0e87ee9664e3ffd931e4e035c36e487f
Shaun Tseng <breeze101792@gmail.com>
2020-03-11 17:07:39 +0000

    [Shell] Refine bash script
    refine bash script

--- scripts/base_bash.sh
+++ scripts/base_bash.sh
@@ -2,14 +2,26 @@
 
 # If not running interactively, don't do anything
 [ -z "$PS1" ] && return
+# case $- in
+#     *i*) ;;
+#       *) return;;
+# esac
 
 # for setting history length see HISTSIZE and HISTFILESIZE in bash(1)
 HISTSIZE=1000
 HISTFILESIZE=2000
 
+# append to the history file, don't overwrite it
+shopt -s histappend
+
 # check the window size after each command and, if necessary,
 # update the values of LINES and COLUMNS.
 shopt -s checkwinsize
+
+# set a fancy prompt (non-color, unless we know we "want" color)
+case "$TERM" in
+    xterm-color|*-256color) color_prompt=yes;;
+esac
 
 # make less more friendly for non-text input files, see lesspipe(1)
 [ -x /usr/bin/lesspipe ] && eval "$(SHELL=/bin/sh lesspipe)"
