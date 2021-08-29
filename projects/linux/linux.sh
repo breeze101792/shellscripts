1cd82a6432e38faf2a0b46c941ce0cd00166c9c4
Shaowu Tseng <breeze101792@gmail.com>
2021-08-29 23:29:47 +0800

    [Git] Refine linux project & git
    [Description]
    Refine linux project & git
    
    [Self-Test]
    N/A

--- projects/linux/linux.sh
+++ projects/linux/linux.sh
@@ -1,14 +1,37 @@
 hs_print "Source Linux(lx,ub) project"
 function lx_ide()
 {
-    local arch=arm64
+    local arch=arm
     local target_dirs=("block" "certs" "crypto" "fs" "include" "init" "ipc" "kernel" "lib" "mm" "net" "security" "virt")
+
+    while [[ "$#" != 0 ]]
+    do
+        case $1 in
+            -a|--arch)
+                arch=$2
+                shift 1
+                ;;
+            -h|--help)
+                cli_helper -c "template" -cd "template function"
+                cli_helper -t "SYNOPSIS"
+                cli_helper -d "template [Options] [Value]"
+                cli_helper -t "Options"
+                cli_helper -o "-a|--arch" -d "Select arch(riscv, x86, arm, arm64, mips), defaut arm"
+                cli_helper -o "-h|--help" -d "Print help function "
+                return 0
+                ;;
+            *)
+                break
+                ;;
+        esac
+        shift 1
+    done
     target_dirs+=("arch/${arch}/")
     pvinit ${target_dirs}
 }
 function ub_ide()
 {
-    local arch=arm64
+    local arch=arm
     local target_dirs=("api" "cmd" "common" "disk" "drivers" "env" "fs" "include" "lib" "net" "post" "test" "tools")
     target_dirs+=("arch/${arch}/")
     pvinit ${target_dirs}
